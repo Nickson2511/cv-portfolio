1de811e03b4320777a34d0e01c06fa48ebddabca
Nickson2511 <okwembanickson8@gmail.com>
2026-06-07 00:43:33 +0300

    add the 3d and animations

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ const NavBar = () => {
     <header className={`navbar ${scrolled ? "scrolled" : "not-scrolled"}`}>
       <div className="inner">
         <a href="#hero" className="logo">
-          Adrian JSM
+          Nickson2511
         </a>
 
         <nav className="desktop">
--- src/constants/index.js
+++ src/constants/index.js
@@ -29,9 +29,9 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
+  { value: 10, suffix: "+", label: "Years of Experience" },
+  { value: 100, suffix: "+", label: "Satisfied Clients" },
+  { value: 30, suffix: "+", label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
 ];
 
@@ -103,8 +103,8 @@ const techStackImgs = [
     imgPath: "/images/logos/node.png",
   },
   {
-    name: "Interactive Developer",
-    imgPath: "/images/logos/three.png",
+    name: "Nodejs Developer",
+    imgPath: "/images/logos/nodejs.png",
   },
   {
     name: "Project Manager",
@@ -132,7 +132,7 @@ const techStackIcons = [
     rotation: [0, -Math.PI / 2, 0],
   },
   {
-    name: "Interactive Developer",
+    name: "Nodejs Developer",
     modelPath: "/models/three.js-transformed.glb",
     scale: 0.05,
     rotation: [0, 0, 0],
@@ -147,37 +147,37 @@ const techStackIcons = [
 
 const expCards = [
   {
-    review: "Adrian brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
-    imgPath: "/images/exp1.png",
+    review: "Nickson brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    imgPath: "/images/tausi.jpeg",
     logoPath: "/images/logo1.png",
-    title: "Frontend Developer",
+    title: "Frontend Developer / Mobile Developer",
     date: "January 2023 - Present",
     responsibilities: [
-      "Developed and maintained user-facing features for the Hostinger website.",
+      "Developed and maintained user-facing features for the Tausi App, the app serving beauty and all of its services at comfort of your home, you just need to book your beauticians while at home then you connect .",
       "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
       "Optimized web applications for maximum speed and scalability.",
     ],
   },
   {
-    review: "Adrian’s contributions to Docker's web applications have been outstanding. He approaches challenges with a problem-solving mindset.",
-    imgPath: "/images/exp2.png",
+    review: "Nickson’s contributions to IYF's weekend academy school management portal  have been outstanding. We came up with a problem to solve for the replacement of manual paper work of managing our weekend academy students from administration to the receiving of services. We have done with premise deployment of the version part of it since it is admin controllable. We are currently building a react native mobile version of it to serve students.",
+    imgPath: "/images/iyf.png",
     logoPath: "/images/logo2.png",
     title: "Full Stack Developer",
     date: "June 2020 - December 2023",
     responsibilities: [
-      "Led the development of Docker's web applications, focusing on scalability.",
+      "Led the development of IYF students portal, focusing on scalability.",
       "Worked with backend engineers to integrate APIs seamlessly with the frontend.",
-      "Contributed to open-source projects that were used with the Docker ecosystem.",
+      "Wired the app with criteria of RBAC,  the admin and users are separated by roles, inside the app a user has access to authorized resource.",
     ],
   },
   {
-    review: "Adrian’s work on Appwrite’s mobile app brought a high level of quality and efficiency. He delivered solutions that enhanced our mobile experience & meet our product goals.",
-    imgPath: "/images/exp3.png",
+    review: "Nickson’s work on Hamisi Bookshop POS   brought a high level of quality and efficiency. The problem was Mr Hamisi had no any selling platform and due to an increase number of buyers, instead of a buyer queeing , he can book online the book and be delivered to him, or if the buyer is already in the physical premises, then the cashier can proceed using the system for operations like scanning prices, recording them automatically, making   accurate calculations, providing detailed receipt records and can scan, Bar code scanner as a feature allows scanning both products and as well users cards .",
+    imgPath: "/images/book-logo.webp",
     logoPath: "/images/logo3.png",
     title: "React Native Developer",
     date: "March 2019 - May 2020",
     responsibilities: [
-      "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
+      "Built cross-platform web apps using React and its ecosystem, integrating with Nodejs's backend services.",
       "Improved app performance and user experience through code optimization and testing.",
       "Coordinated with the product team to implement features based on feedback.",
     ],
@@ -201,45 +201,45 @@ const expLogos = [
 
 const testimonials = [
   {
-    name: "Esther Howard",
-    mentions: "@estherhoward",
+    name: "Solomon ",
+    mentions: "@iyf weekend Academy",
     review:
-      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+      "I can’t say enough good things about Nickson. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding. IYF Weekend Academy is now able to reach a wider audience and provide a better user experience thanks to Nickson's work.",
     imgPath: "/images/client1.png",
   },
   {
     name: "Wade Warren",
     mentions: "@wadewarren",
     review:
-      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+      "Working with Nickson was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects/mobile development.",
     imgPath: "/images/client3.png",
   },
   {
-    name: "Guy Hawkins",
-    mentions: "@guyhawkins",
+    name: "Hamisi Henry",
+    mentions: "@hamisiBookshop",
     review:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Nickson was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Nickson's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Nickson is the ideal partner, we delivered with him an Enterprise point of sale solution for Hamisi Bookshop.",
     imgPath: "/images/client2.png",
   },
   {
-    name: "Marvin McKinney",
-    mentions: "@marvinmckinney",
+    name: "Nick Nickson",
+    mentions: "@GreenLeaf Investments",
     review:
-      "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+      "Nickson was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
     imgPath: "/images/client5.png",
   },
   {
-    name: "Floyd Miles",
-    mentions: "@floydmiles",
+    name: "Kevin ",
+    mentions: "@kevmwas",
     review:
-      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+      "Nickson’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
     imgPath: "/images/client4.png",
   },
   {
-    name: "Albert Flores",
-    mentions: "@albertflores",
+    name: "Timothy Mugo",
+    mentions: "@Tausi App",
     review:
-      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+      "Nickson was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
     imgPath: "/images/client6.png",
   },
 ];
